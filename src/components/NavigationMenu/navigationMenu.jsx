4d2af1d7a95475400000a0ea3d3b9b7423bba0b5
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-10-17 00:29:04 -0300

    Creation of the related section in the left menu

--- src/components/NavigationMenu/navigationMenu.jsx
+++ src/components/NavigationMenu/navigationMenu.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 import { AiFillHome, AiFillFire } from 'react-icons/ai'
-import { RiVideoLine } from 'react-icons/ri';
+import { RiVideoLine } from 'react-icons/ri'
+import { MdVideoLibrary, MdHistory, MdOndemandVideo } from 'react-icons/md'
+import { FiClock } from 'react-icons/fi'
 
 import './navigationMenuStyle.scss'
 
 const NavigationMenu = () => {
     return (
         <aside>
-            <div className="main-sections">
+            <section className="main-sections">
                 <div className="main-sections-item">
                     <AiFillHome size={20} />
                     <span>
@@ -22,12 +24,39 @@ const NavigationMenu = () => {
                     </span>
                 </div>
                 <div className="main-sections-item">
-                    <RiVideoLine size={20} />
+                    <MdOndemandVideo size={20} />
                     <span>
                         Inscrições
                     </span>
                 </div>
-            </div>
+            </section>
+
+            <section className="related-sections">
+                <div className="related-sections-item">
+                    <MdVideoLibrary size={20} />
+                    <span>
+                        Biblioteca
+                    </span>
+                </div>
+                <div className="related-sections-item">
+                    <MdHistory size={20} />
+                    <span>
+                        Histórico
+                    </span>
+                </div>
+                <div className="related-sections-item">
+                    <RiVideoLine size={20} />
+                    <span>
+                        Seus vídeos
+                    </span>
+                </div>
+                <div className="related-sections-item">
+                    <FiClock size={20} />
+                    <span>
+                        Assistir mais tarde
+                    </span>
+                </div>
+            </section>
         </aside>
     )
 }
